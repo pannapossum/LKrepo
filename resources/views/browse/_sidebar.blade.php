33f05783e4a40e9e33542b15fa0ed8db295f9cb6
jam <ch3rvbcreations@gmail.com>
2024-04-29 03:36:59 -0400

    no message

--- resources/views/browse/_sidebar.blade.php
+++ resources/views/browse/_sidebar.blade.php
@@ -5,7 +5,7 @@
         <div class="sidebar-item"><a href="{{ url('masterlist') }}" class="{{ set_active('masterlist*') }}">Characters</a></div>
         <div class="sidebar-item"><a href="{{ url('myos') }}" class="{{ set_active('myos*') }}">MYO Slots</a></div>
     </li>
-    @if (Settings::get('character_likes_leaderboard_enable'))
+    @if (Settings::get('character_likes_leaderboard_enable') && Auth::check())
     <li class="sidebar-section">
         <div class="sidebar-section-header">Character {{ ucfirst(__('character_likes.likes')) }}</div>
         <div class="sidebar-item"><a href="{{ url(__('character_likes.likes').'-leaderboard') }}" class="{{ set_active(__('character_likes.likes').'-leaderboard*') }}">{{ ucfirst(__('character_likes.likes')) }} Leaderboard</a></div>
