1ab2bf8885df62491c532bcb1ae919fd5b5369f6
camyza <rayesesshyfan@gmail.com>
2024-12-15 03:59:16 -0600

    Profile header image v3 code since different than v2's.

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable implements MustVerifyEmail {
     protected $fillable = [
         'name', 'alias', 'rank_id', 'email', 'email_verified_at', 'password', 'is_news_unread', 'is_banned', 'has_alias', 'avatar', 'is_sales_unread', 'birthday',
         'is_deactivated', 'deactivater_id',
+        'profile_img',
     ];
 
     /**
@@ -380,6 +381,15 @@ class User extends Authenticatable implements MustVerifyEmail {
     }
 
     /**
+    * Displays the user's profile image.
+    *
+    * @return string
+    */
+    public function getProfileImg() {
+        return $this->profile_img;
+    }
+
+    /**
      * Gets the display URL for a user's avatar, or the default avatar if they don't have one.
      *
      * @return url
@@ -400,6 +410,20 @@ class User extends Authenticatable implements MustVerifyEmail {
 
         return url('images/avatars/'.$this->avatar.'?v='.filemtime(public_path('images/avatars/'.$this->avatar)));
     }
+
+    /**
+    * Gets the display URL for a user's profile image, or the default profile image if they don't have one.
+    *
+    * @return url
+    */
+    public function getProfileImgUrlAttribute() {
+        if ($this->profile_img == 'default.png') {
+            return url('images/profileimgs/default.png');
+        }
+
+        return url('images/profileimgs/' . $this->profile_img);
+    }
+
 
     /**
      * Gets the user's log type for log creation.
