780c7fdcaea84ebb35790c02f436c418c8cdcfb2
camyza <rayesesshyfan@gmail.com>
2024-12-15 11:19:55 -0600

    add: ext-tracker file

---
+++ config/lorekeeper/ext-tracker/profile_header_image.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'wiki_key' => 'Profile Header Image',
+    'creators' => json_encode([
+        'Camy' => 'https://github.com/Camyza',
+    ]),
+    'version'  => '1.0.0',
+];
